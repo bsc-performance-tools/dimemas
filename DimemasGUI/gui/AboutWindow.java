a41a9694ab6dff229fd4b52167ee6a2744ac3086
Juan González <juan.gonzalez@bsc.es>
2010-03-10 09:33:36 +0000

    Minor Changes in the GUI

--- DimemasGUI/gui/AboutWindow.java
+++ DimemasGUI/gui/AboutWindow.java
@@ -1,3 +1,36 @@
+/*****************************************************************************\
+ *                        ANALYSIS PERFORMANCE TOOLS                         *
+ *                               Dimemas GUI                                 *
+ *                  GUI for the Dimemas simulation tool                      *
+ *                                                                           * 
+ *****************************************************************************
+ *     ___     This library is free software; you can redistribute it and/or *
+ *    /  __         modify it under the terms of the GNU LGPL as published   *
+ *   /  /  _____    by the Free Software Foundation; either version 2.1      *
+ *  /  /  /     \   of the License, or (at your option) any later version.   *
+ * (  (  ( B S C )                                                           *
+ *  \  \  \_____/   This library is distributed in hope that it will be      *
+ *   \  \__         useful but WITHOUT ANY WARRANTY; without even the        *
+ *    \___          implied warranty of MERCHANTABILITY or FITNESS FOR A     *
+ *                  PARTICULAR PURPOSE. See the GNU LGPL for more details.   *
+ *                                                                           *
+ * You should have received a copy of the GNU Lesser General Public License  *
+ * along with this library; if not, write to the Free Software Foundation,   *
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA          *
+ * The GNU LEsser General Public License is contained in the file COPYING.   *
+ *                                 ---------                                 *
+ *   Barcelona Supercomputing Center - Centro Nacional de Supercomputacion   *
+\*****************************************************************************/
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- *\
+
+  $URL::              $:  File
+  $Rev::              $:  Revision of last commit
+  $Author::           $:  Author of last commit
+  $Date::             $:  Date of last commit
+
+\* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+
 package gui;
 
 /**
@@ -51,25 +84,33 @@ public class AboutWindow extends JFrame implements ActionListener
   */
   private JPanel aboutPanel()
   {
-    JPanel panel = new JPanel(new GridLayout(11,1));
+    JPanel panel = new JPanel(new GridLayout(7,1));
 
-    panel.setBorder(BorderFactory.createEtchedBorder(1));
-    panel.add(new JLabel(""));
-    panel.add(new JLabel(
-      "     DIMEMAS is a performance analysis tool for message passing     "));
-    panel.add(new JLabel(
-      "     programs.  It is designed,  developed  and  maintained  by  the"));
-    panel.add(new JLabel(
-      "     Centro Europeo de Paralelismo de Barcelona of the Technical"));
-    panel.add(new JLabel("     University of Catalonia."));
-    panel.add(new JLabel(""));
-    panel.add(new JLabel("Contact:  support@cepba.upc.es",JLabel.CENTER));
-    panel.add(new JLabel(""));
-    panel.add(new JLabel(
-      "For further information visit the Dimemas Web Site at:",JLabel.CENTER));
-    panel.add(new JLabel("http://www.cepba.upc.es/dimemas",JLabel.CENTER));
+    // panel.setBorder(BorderFactory.createEtchedBorder(1));
+
     panel.add(new JLabel(""));
 
+    panel.add(
+      new JLabel(
+        "DIMEMAS is a performance analysis tool for message passing",
+        JLabel.CENTER));
+    
+    panel.add(
+      new JLabel(
+        "programs.  It is designed,  developed  and  maintained  by  the",
+        JLabel.CENTER));
+
+    panel.add(
+      new JLabel(
+        "Barcelona Supercomputing Center - Centro Nacional de Supercomputación",
+        JLabel.CENTER));
+
+    panel.add(new JLabel(""));
+    
+    panel.add(new JLabel("Contact:  tools@bsc.es",JLabel.CENTER));
+
+    panel.add(new JLabel(""));
+    
     return panel;
   }
 
@@ -84,7 +125,7 @@ public class AboutWindow extends JFrame implements ActionListener
     panelPrincipal.setLayout(new BorderLayout());
 
     // Se añaden los elementos de la ventana.
-    panelPrincipal.add("West",new JLabel(new ImageIcon(Data.ICON_IMAGE)));
+    panelPrincipal.add("North",new JLabel(new ImageIcon(Data.ICON_IMAGE)));
     panelPrincipal.add("Center",aboutPanel());
     panelPrincipal.add("South",b_close);
 
