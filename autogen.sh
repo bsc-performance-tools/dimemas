3d60b6e1238feee7648441eb773825722c41fdac
Juan González <juan.gonzalez@bsc.es>
2012-09-03 10:17:22 +0000

    Added a missing header (basic_types.h) and changed the building numbering system.

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 MAJOR=4
 MINOR=0
+REV=1
 sh -c "svnversion -n . | cut -d: -f2 | cut -dS -f1 | tr -d '\n'" > ./SVN_VERSION
 SVN_VERSION=`cat ./SVN_VERSION`
-VERSION="$MAJOR.$MINOR-r$SVN_VERSION"
+VERSION="$MAJOR.$MINOR.$REV-r$SVN_VERSION"
 
 if test -e ./configure.ac; then
   rm ./configure.ac
