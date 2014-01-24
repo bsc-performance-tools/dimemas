0b35cc7313e9525dd56a75c4ec5a4dec29b946cf
Juan González <juan.gonzalez@bsc.es>
2014-01-24 12:30:30 +0000

    New release of Dimemas, with the predefined mappings and new parameters

---
+++ 5.2.0/Simulator/input/ts.h
@@ -0,0 +1,81 @@
+/*****************************************************************************\
+ *                        ANALYSIS PERFORMANCE TOOLS                         *
+ *                                  Dimemas                                  *
+ *       Simulation tool for the parametric analysis of the behaviour of     *
+ *       message-passing applications on a configurable parallel platform    *
+ *                                                                           *
+ *****************************************************************************
+ *     ___     This library is free software; you can redistribute it and/or *
+ *    /  __         modify it under the terms of the GNU LGPL as published   *
+ *   /  /  _____    by the Free Software Foundation; either version 2.1      *
+ *  /  /  /     \   of the License, or (at your option) any later version.   *
+ * (  (  ( B S C )                                                           *
+ *  \  \  \_____/   This library is distributed in hope that it will be      *
+ *   \  \__         useful but WITHOUT ANY WARRANTY; without even the        *
+ *    \___          implied warranty of MERCHANTABILITY or FITNESS FOR A     *
+ *                  PARTICULAR PURPOSE. See the GNU LGPL for more details.   *
+ *                                                                           *
+ * You should have received a copy of the GNU Lesser General Public License  *
+ * along with this library; if not, write to the Free Software Foundation,   *
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA          *
+ * The GNU LEsser General Public License is contained in the file COPYING.   *
+ *                                 ---------                                 *
+ *   Barcelona Supercomputing Center - Centro Nacional de Supercomputacion   *
+\*****************************************************************************/
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- *\
+
+  $URL::                  $:  File
+  $Rev::                  $:  Revision of last commit
+  $Author::               $:  Author of last commit
+  $Date::                 $:  Date of last commit
+
+\* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+
+#ifndef _TS_H_
+#define _TS_H_
+
+struct Str
+{
+  char           *str;
+  struct Str     *next_str;
+};
+
+struct t_entry
+{
+  int             entryid;
+  char           *name;
+  struct t_queue *types;
+};
+
+struct t_element
+{
+   struct t_array *i3;
+   int             type;
+};
+
+struct t_array
+{
+  char *i1;
+  int   dimension;
+};
+
+struct t_arr
+{
+  struct t_queue *q;
+  int             dim1, dim2;
+};
+
+struct t_field
+{
+  int             tipo;
+  union
+  {
+    int          dec;
+    double       real;
+    char        *string;
+    struct t_arr arr;
+  } value;
+};
+
+#endif
---
+++ 5.2.0/common_utilities/error.h
@@ -0,0 +1,4 @@
+
+int generate_error(__const char**__restrict __buffer, __const char*__restrict __format, ...);
+
+
