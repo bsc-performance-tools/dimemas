1a9477090e87a88f51d843018c4509f3806b3383
Juan Francisco Martínez Vera <juan.martinez@bsc.es>
2017-10-23 16:31:10 +0200

    get_thread_by_taskid is slow function, it is mainly used when clean-deadlocks option is set. Now it will be called just with clean-deadlock option set. OCLEventEncodint_Is_OCLBlock now is only going to be called when dealing with GPU accelerated application' traces
    Change-Id: I21f55d22c6c2e11ad0f0f88280a37bac519ee6c6

--- Simulator/sched/schedule.c
+++ Simulator/sched/schedule.c
@@ -749,9 +749,18 @@ void SCHEDULER_general (int value, struct t_thread *thread)
                     }
                     else
                     {
+                        if (!thread->task->accelerator)
+                        {	/*	It's a CPU burst	*/
+                            PARAVER_Running (cpu->unique_number,
+                                    IDENTIFIERS (thread),
+                                    thread->last_paraver,
+                                    current_time);
+                        }
+
                         if (thread->kernel && (!thread->first_acc_event_read 
                                     || thread->acc_in_block_event.value == 0))
-                        {	/* Previous at accelerator events in kernel thread must be NOT_CREATED state in CPU	*/
+                        {	
+                            /* Previous at accelerator events in kernel thread must be NOT_CREATED state in CPU	*/
                             /* Not created states between blocks in kernel thread	*/
                             PARAVER_Not_Created(cpu->unique_number,
                                     IDENTIFIERS (thread),
@@ -761,7 +770,8 @@ void SCHEDULER_general (int value, struct t_thread *thread)
                         else if (thread->kernel 
                                 && (CUDAEventEconding_Is_CUDALaunch(thread->acc_in_block_event) 
                                     || OCLEventEncoding_Is_OCLKernelRunning(thread->acc_in_block_event)))
-                        {	/*	It's a GPU burst	*/
+                        {	
+                            /*	It's a GPU burst	*/
                             PARAVER_Running(cpu->unique_number,
                                     IDENTIFIERS (thread),
                                     thread->last_paraver,
