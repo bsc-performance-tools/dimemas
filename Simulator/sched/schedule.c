17ff2aa5a5472fe6efbe2085a0209374d29b9a9b
dbernal <david.bernal@bsc.es>
2024-12-12 14:15:06 +0100

    Removes original durations of CUDA memory transfers in CUDA simulations. Its duration now only includes: the synchronization time + the transfer time.

--- Simulator/sched/schedule.c
+++ Simulator/sched/schedule.c
@@ -461,6 +461,7 @@ t_nano SCHEDULER_get_execution_time( struct t_thread *thread )
   
   /* for ideal configuration ignore the burst inside the configure call */
   if ( ( thread->stream && CUDAEventEconding_Is_CUDAConfigCall ( thread->acc_in_block_event ) && thread->task->node->acc.startup == 0 ) ||
+       ( CUDAEventEncoding_Is_CUDATransferBlock ( thread->acc_in_block_event ) && simulate_cuda) ||
        ( is_ideal_openmp == TRUE &&
          !OMPEventEncoding_Is_OMPWorker_Running( thread->omp_in_block_event ) &&
          ( OMPEventEncoding_Is_OMPBlock( thread->omp_in_block_event.type ) && thread->omp_in_block_event.value > 0 ||
