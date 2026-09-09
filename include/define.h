57967205663f64a88c028d2fb53dc9127ead81b8
Pedro Antonio Gonzalez Navarro <pedro.gonzalez@bsc.es>
2026-09-09 18:35:38 +0200

    Changed enum with collectives to match DimemasGUI.

--- include/define.h
+++ include/define.h
@@ -551,19 +551,19 @@ enum
   MPI_Allgatherv,
   MPI_Alltoall,
   MPI_Alltoallv,
-  MPI_Reduce, /* 10 */
+  MPI_Alltoallw, /* 10 */
+  MPI_Reduce,
   MPI_Allreduce,
   MPI_Reduce_Scatter,
   MPI_Reduce_Scatter_block,
   MPI_Scan,
-  MPI_Alltoallw,
   MPI_Exscan
 };
 
 static const char* Global_Ops_Labels[ GLOBAL_OPS_COUNT ] = {
-  "MPI_Barrier",    "MPI_Bcast",    "MPI_Gather",    "MPI_Gatherv", "MPI_Scatter",   "MPI_Scatterv",       "MPI_Allgather",
-  "MPI_Allgatherv", "MPI_Alltoall", "MPI_Alltoallv", "MPI_Reduce",  "MPI_Allreduce", "MPI_Reduce_Scatter", "MPI_Reduce_Scatter_block",
-  "MPI_Scan",       "MPI_Alltoallw", "MPI_Exscan"
+  "MPI_Barrier",    "MPI_Bcast",    "MPI_Gather",    "MPI_Gatherv", "MPI_Scatter",   "MPI_Scatterv",  "MPI_Allgather",
+  "MPI_Allgatherv", "MPI_Alltoall", "MPI_Alltoallv", "MPI_Alltoallw", "MPI_Reduce",  "MPI_Allreduce", "MPI_Reduce_Scatter", "MPI_Reduce_Scatter_block",
+  "MPI_Scan",       "MPI_Exscan"
 };
 
 #define ACCELERATOR_NULL   0 // No accelerator tracing
